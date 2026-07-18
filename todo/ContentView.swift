e063820eeb014f03feb3b7465247cf799ad84301
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2026-07-18 08:50:39 +0600

    Initial Commit

---
+++ todo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todo
+//
+//  Created by Sabuj on 18/7/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todo/todoApp.swift
@@ -0,0 +1,17 @@
+//
+//  todoApp.swift
+//  todo
+//
+//  Created by Sabuj on 18/7/26.
+//
+
+import SwiftUI
+
+@main
+struct todoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
